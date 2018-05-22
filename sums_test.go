9c020333e24b7b3e8387c8f1f873bddb059873d3
Vinh Vu <vinh@Vinhs-MacBook-Pro.local>
2018-05-22 09:57:09 -0400

    lets try this again

--- sums_test.go
+++ sums_test.go
@@ -7,7 +7,7 @@ func TestSum(t *testing.T) {
     if total != 10 {
        t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
     }
-    total := Sum(8,8)
+    total = Sum(8,8)
     if total != 16 {
        t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 16)
     }
