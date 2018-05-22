062e7135502c4c3c666658617fda9f8ec58a1683
Vinh Vu <vinh@Vinhs-MacBook-Pro.local>
2018-05-22 09:53:28 -0400

    2 language test

--- sums_test.go
+++ sums_test.go
@@ -7,4 +7,9 @@ func TestSum(t *testing.T) {
     if total != 10 {
        t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
     }
+    total := Sum(8,8)
+    if total != 16 {
+       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 16)
+    }
+
 }
