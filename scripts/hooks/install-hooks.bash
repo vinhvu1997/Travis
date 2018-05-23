8db5826279995edc30cd374c590138c1a0ee0eb6
Vinh Vu <vinh@Vinhs-MacBook-Pro.local>
2018-05-23 10:33:11 -0400

    testing

--- scripts/hooks/install-hooks.bash
+++ scripts/hooks/install-hooks.bash
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 GIT_DIR=$(git rev-parse --git-dir)
-
 echo "Installing hooks..."
 # this command creates symlink to our pre-push script
-ln -s ../scripts/hooks/pre-push.bash $GIT_DIR/hooks/pre-push
+ln -s $GOPATH/src/Travis/scripts/hooks/pre-push.bash $GIT_DIR/hooks/pre-commit
 echo "Done!"
